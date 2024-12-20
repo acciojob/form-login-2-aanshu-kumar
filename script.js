d1bbb690bfd72ff296b44d30073ba09386e092c0
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-20 19:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function submit_btn() {
+	let f_name=documet.getElementById("f_name");
+	let l_name=documet.getElementById("l_name");
+	let phone=documet.getElementById("phone_no");
+	let Email=documet.getElementById("Email");
+	alert(`First Name: ${f_name.textContent} Last Name: ${l_name.textContent} Phone Number: ${phone_no.textContent} Email ID: ${Email.textContent}`)
+}
