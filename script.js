e802df732de31c1c02c4e69fc791318263542c38
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-20 21:12:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 
 function submit_btn() {
-	let f_name=document.getElementById("f_name");
-	let l_name=document.getElementById("l_name");
-	let phone=document.getElementById("phone_no");
-	let Email=document.getElementById("Email");
-	alert(`First Name: ${f_name.value} Last Name: ${l_name.value} Phone Number: ${phone.value} Email ID: ${Email.value}`)
+	let f_name = document.getElementById("f_name").value;
+	let l_name = document.getElementById("l_name").value;
+	let phone = document.getElementById("phone_no").value;
+	let Email = document.getElementById("Email").value;
+	alert(`First Name: ${f_name} \nLast Name: ${l_name} \nPhone Number: ${phone} \nEmail ID: ${Email}`)
 }
